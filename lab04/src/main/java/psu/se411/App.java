a9700b78c0f111c944db8abcc8fc02ac663b0194
Norahaljasser-eng <222410205@psu.edu.sa>
2026-09-10 17:57:22 +0300

    Add Lab04

---
+++ lab04/src/main/java/psu/se411/App.java
@@ -0,0 +1,21 @@
+package psu.se411;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            primaryStage.setTitle("SE411");
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
